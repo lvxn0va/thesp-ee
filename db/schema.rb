281d5cefe0057811a77e4d7335cc1455897bd2d4
lvxn0va <lvx.n0va@gmail.com>
2012-09-09 18:07:20 -0700

    Journal Botstrap CSS

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120907061438) do
+ActiveRecord::Schema.define(:version => 20120909190242) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(:version => 20120907061438) do
   add_index "roles", ["name"], :name => "index_roles_on_name"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -35,9 +35,11 @@ ActiveRecord::Schema.define(:version => 20120907061438) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.string   "name"
+    t.boolean  "admin"
+    t.boolean  "verified",               :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
